8de519596cb483cabfa2b50c6fa8a49102169c31
yexnsxo <yslove829@naver.com>
2026-07-06 21:10:23 +0900

    feat: Entry CRUD ViewSet 구현

--- entries/views.py
+++ entries/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Entry
+from .serializers import EntrySerializer
 
-# Create your views here.
+class EntryViewSet(viewsets.ModelViewSet):
+  queryset = Entry.objects.all()
+  serializer_class = EntrySerializer
