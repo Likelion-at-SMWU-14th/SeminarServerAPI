40d8baba2d822cf07fb34c30044d0daca35d861d
yexnsxo <yslove829@naver.com>
2026-07-08 01:06:04 +0900

    feat: 로그인 사용자만 글 작성 및 author 자동 지정

--- entries/serializers.py
+++ entries/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Entry
 
 class EntrySerializer(serializers.ModelSerializer):
+  author = serializers.StringRelatedField(read_only=True)
   class Meta:
     model = Entry
     fields = ['id', 'author', 'comment', 'timestamp']
--- entries/views.py
+++ entries/views.py
@@ -1,7 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Entry
 from .serializers import EntrySerializer
 
 class EntryViewSet(viewsets.ModelViewSet):
   queryset = Entry.objects.all()
   serializer_class = EntrySerializer
+  permission_classes = [IsAuthenticatedOrReadOnly]
+
+  def perform_create(self, serializer):
+    serializer.save(author=self.request.user)
