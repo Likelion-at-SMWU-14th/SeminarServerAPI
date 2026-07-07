c8f636306eb89fd080d9f0832e4fcccd996e3ffa
yexnsxo <yslove829@naver.com>
2026-07-07 20:07:52 +0900

    feat: Entry author를 User ForeignKey로 전환

--- entries/models.py
+++ entries/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Entry(models.Model):
-  author = models.CharField(max_length=50)
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
   comment = models.TextField()
   timestamp = models.DateTimeField(auto_now_add=True)
 
