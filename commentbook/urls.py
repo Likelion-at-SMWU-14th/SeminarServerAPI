69b46a93c3ee7b1fa8e1d624879ef92899d0b3b3
yexnsxo <yslove829@naver.com>
2026-07-08 22:09:19 +0900

    refactor: 토큰 발급 URL을 /login/으로 변경

--- commentbook/urls.py
+++ commentbook/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshVie
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('entries.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='login_refresh'),
     path('api-auth/', include('rest_framework.urls')),
 ]
